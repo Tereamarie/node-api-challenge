ac3e84d2693a2518ce0a3da46265255d5b157e51
marie-hotchkiss <mtknowlton67@gmail.com>
2020-07-17 18:55:53 -0700

    Added files and folder

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+const server = require("./server");
+
+const port = 1333;
+server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
